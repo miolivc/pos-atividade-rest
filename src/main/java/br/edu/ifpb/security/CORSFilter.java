0bda3bd75dc26956ede5c73556fcdd40ec8914a4
Michelle Oliveira <miolivc@outlook.com>
2018-04-06 15:42:14 -0300

    filtro cross-origin

---
+++ src/main/java/br/edu/ifpb/security/CORSFilter.java
@@ -0,0 +1,29 @@
+package br.edu.ifpb.security;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+@PreMatching
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
+
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Access-Control-Allow-Headers", "accept, origin, X-Requested-With,"
+                + " Content-Type, X-Codingpedia, Authorization");
+        headers.add("Access-Control-Max-Age", "172800");
+    }
+}
+
